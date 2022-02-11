3715deffe77b7a59cb0648af091dc16c26998eb6
Shakana Jamal <shakana.jamal.moalin@iths.se>
2022-02-11 15:11:21 +0100

    Added Cart solving total function in store

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,11 +1,51 @@
 <template>
   <div class="cart-wrapper">
     <h1>Din beställning</h1>
+    <main>
+      <ul v-for="order in orders" :key="order.id">
+        <li>{{ order.title }}</li>
+        <li>{{ order.price }} kr</li>
+      </ul>
+      <p>{{ Total }}</p>
+    </main>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    orders() {
+      return this.$store.state.orderList;
+    },
+    Total() {
+      return this.$store.getters.setTotal;
+    },
+  },
+};
 </script>
 
-<style></style>
+<style>
+main {
+  display: flex;
+  flex-direction: column;
+}
+ul {
+  margin: 1rem 2rem 1rem 1rem;
+  align-self: flex-start;
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+}
+li {
+  margin: 0;
+  padding: 0;
+}
+li:nth-of-type(2) {
+  width: 40px;
+}
+
+ul li:nth-child(1) {
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+</style>
