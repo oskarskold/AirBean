86f0a27a4e18315517778f27765079d9e6aec55f
oskarskold <Oskar.skold4@hotmail.com>
2022-02-11 09:31:03 +0100

    Add new repo

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Meny</h1>
+    <div class="container-menu">
+      <ul v-for="m in menu" :key="m.id">
+        <div class="wrapper-menu">
+          <li>{{ m.title }}</li>
+          <li>{{ m.price }}</li>
+        </div>
+        <p>{{ m.desc }}</p>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  computed: {
+    menu() {
+      return this.$store.state.menu.menu;
+    },
   },
 };
 </script>
+
+<style scoped>
+.home {
+  background: #f3e4e1;
+}
+h1 {
+  color: #2f2926;
+}
+
+li {
+  color: #2f2926;
+  margin: 0 1rem;
+  font-weight: 700;
+}
+p {
+  color: #2f2926;
+}
+ul {
+  list-style-type: none;
+}
+.wrapper-menu {
+  display: flex;
+}
+.container-menu {
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+}
+</style>
