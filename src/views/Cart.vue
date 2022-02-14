035cf2699a0949b933ef0031b9ec7ff45cbd2b03
oskarskold <Oskar.skold4@hotmail.com>
2022-02-14 19:56:50 +0100

    Add amount for each coffee type We wanna be able to see how many coffees we order.

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -5,9 +5,10 @@
       <ul v-for="order in orders" :key="order.id">
         <li>{{ order.title }}</li>
         <li>{{ order.price }} kr</li>
+        <li>{{ order.amount }} st</li>
       </ul>
 
-      <p>{{ Total }}</p>
+      <p>{{ Total }} kr</p>
     </main>
   </div>
 </template>
